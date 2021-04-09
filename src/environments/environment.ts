dc429255af737a7b005adcbfd1201bdc3148dfb2
kervin <dasu15@hotmail.com>
2021-04-09 10:43:44 -0600

    All

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,21 @@
 export const environment = {
-    tenant: 'CHANGEIT',
+    tenant: 'tilechat',
     supportMode: true,
-    CHAT_SEND_BY_EMAIL_LINK: 'CHANGEIT',
-    FIREBASESTORAGE_BASE_URL_IMAGE: 'CHANGEIT',
-    DASHBOARD_URL: 'CHANGEIT',
-    SERVER_BASE_URL: 'CHANGEIT',
+    CHAT_SEND_BY_EMAIL_LINK: 'mailto:?subject=Transcripcion de la conversacion del chat&body=Hola,%0D%0A%0D%0Aaqui esta la transcripcion:%0D%0A%0D%0Ahttps://tiledesk.solucionesinduso.com/api/public/requests/',
+    FIREBASESTORAGE_BASE_URL_IMAGE: 'https://firebasestorage.googleapis.com/v0/b/',
+    DASHBOARD_URL: 'http://localhost:8080/',
+    SERVER_BASE_URL: 'http://localhost:3000/',
     production: false,
-    remoteConfig: true,
+    remoteConfig: false,
     remoteConfigUrl: './chat-config.json',
-    remoteContactsUrl: '',
     firebaseConfig: {
-        apiKey: 'CHANGEIT',
-        authDomain: 'CHANGEIT',
-        databaseURL: 'CHANGEIT',
-        projectId: 'CHANGEIT',
-        storageBucket: 'CHANGEIT',
-        messagingSenderId: 'CHANGEIT',
-        appId: 'CHANGEIT',
-        chat21ApiUrl: 'CHANGEIT'
+        apiKey: 'AIzaSyCCXlqBMquWntJLBseu6qNTM-mTrMfYFW8',
+        authDomain: 'tiledesk-facture-dev.firebaseapp.com',
+        databaseURL: 'https://tiledesk-facture-dev-default-rtdb.firebaseio.com',
+        projectId: 'tiledesk-facture-dev',
+        storageBucket: 'tiledesk-facture-dev.appspot.com',
+        messagingSenderId: '82084183603',
+        appId: '1:82084183603:web:d04b7e390ddd2d8b342d46',
+        chat21ApiUrl: 'https://us-central1-tiledesk-facture-dev.cloudfunctions.net'
     }
-};
+}
